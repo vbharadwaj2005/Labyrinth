956847ad6abed3534646eb5e0eb09891019008b1
bharadwaj varadan <bharadwajisgc2005@gmail.com>
2022-12-28 13:30:46 +0530

    tenth commit

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ var x = w.innerWidth || e.clientWidth || g.clientWidth,
         y = w.innerHeight || e.clientHeight || g.clientHeight;
 
 var width = 1000;
-    height = 450;
+    height = 500;
 
 var currentPosition; 
 
